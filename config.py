83e43461410add4d8b3f8e3349ac90159eee5cad
tcz <ffmaer@gmail.com>
2020-12-23 04:25:54 +0800

    dataset_path

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ nsf = 4 # encoded voxel size, scale factor
 nvx = 32 # output voxel size
 batch_size = 16
 learning_rate = 2e-4
-dataset_path = "/content/drive/My Drive/ubuntu-live/stars156-binvox/*.binvox"
+dataset_path = "/content/drive/My Drive/v2/ubuntu-gtx1080/stars156-binvox/*.binvox"
 params_path = "params/voxel_dcgan_model.ckpt"
 out_path = "/content/drive/My Drive/colab/out"
