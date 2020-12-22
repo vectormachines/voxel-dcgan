3ab2ceda8558a8599f85e47e920927dfe4edf5d0
tcz <ffmaer@gmail.com>
2020-12-23 04:33:26 +0800

    out_path

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ batch_size = 16
 learning_rate = 2e-4
 dataset_path = "/content/drive/MyDrive/v2/ubuntu-gtx1080/stars156-binvox/*.binvox"
 params_path = "params/voxel_dcgan_model.ckpt"
-out_path = "/content/drive/My Drive/colab/out"
+out_path = "/content/drive/MyDrive/colab/out"
